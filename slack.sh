70b435c61c66461668c3ff138b7d4d64a3bd93a5
Robin Härle <robin@blckct.io>
2023-08-14 16:00:11 +0200

    Fix script for POSIX

--- slack.sh
+++ slack.sh
@@ -4,9 +4,9 @@ set -oue pipefail
 
 # Wayland support is enabled by default, it can be disabled by changing the environment variable in flatseal or by running:
 # flatpak override --user --env=USE_WAYLAND=0 com.slack.Slack
-declare -i USE_WAYLAND="${USE_WAYLAND:-1}"
+USE_WAYLAND="${USE_WAYLAND:-1}"
 
-if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE:-''}" == "wayland" ]]; then
+if [ "${USE_WAYLAND}" -eq 1 ] && [ "${XDG_SESSION_TYPE:-''}" = "wayland" ]; then
 	EXTRA_ARGS="--enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --enable-wayland-ime --ozone-platform=wayland"
 else
 	EXTRA_ARGS="--enable-features=WebRTCPipeWireCapturer"
