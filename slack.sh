05a14e815f7ee885ca881a6a4027bc57c6d11b03
Robin Härle <robin@blckct.io>
2023-08-14 15:46:45 +0200

    Enable native Wayland support by default

--- slack.sh
+++ slack.sh
@@ -2,16 +2,14 @@
 
 set -oue pipefail
 
-export TMPDIR=$XDG_CACHE_HOME
+# Wayland support is enabled by default, it can be disabled by changing the environment variable in flatseal or by running:
+# flatpak override --user --env=USE_WAYLAND=0 com.slack.Slack
+declare -i USE_WAYLAND="${USE_WAYLAND:-1}"
 
-# Wayland support can be optionally enabled by changing the environment variable in flatseal or by running:
-# flatpak override --user --env=USE_WAYLAND=1 com.Slack.slack
-declare -i USE_WAYLAND="${USE_WAYLAND:-0}"
-
-if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
-    EXTRA_ARGS="--enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland"
+if [[ "${USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE:-''}" == "wayland" ]]; then
+	EXTRA_ARGS="--enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --enable-wayland-ime --ozone-platform=wayland"
 else
-    EXTRA_ARGS="--enable-features=WebRTCPipeWireCapturer"
+	EXTRA_ARGS="--enable-features=WebRTCPipeWireCapturer"
 fi
 
-zypak-wrapper /app/extra/bin/slack -s $EXTRA_ARGS "$@"
+zypak-wrapper /app/extra/bin/slack -s "${EXTRA_ARGS}" "$@"
