65eddb76a642b1ef206bd147d98a52156dcd3f57
Pedro Vanzella <pedro@pedrovanzella.com>
2024-03-09 08:28:38 -0300

    Restore detection in older systems

--- slack.sh
+++ slack.sh
@@ -2,7 +2,9 @@
 
 EXTRA_ARGS='--enable-features=WebRTCPipeWireCapturer'
 
-if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET:-'wayland-0'}" || -e "${WAYLAND_DISPLAY}" ]]
+WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
+
+if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
 then
     EXTRA_ARGS="${EXTRA_ARGS} --enable-wayland-ime --ozone-platform-hint=auto"
 fi
