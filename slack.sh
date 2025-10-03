07ec875b235e46121c3f73831aa9d79a2f414875
Aksel Stokseth <aksel.stokseth@gmail.com>
2025-10-03 10:00:36 +0200

    Work around Electron's broken Wayland detection
    Inspired by flathub/org.signal.Signal#965.
    
    Fixes #356.

--- slack.sh
+++ slack.sh
@@ -4,9 +4,14 @@ WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
 
 EXTRA_ARGS='--enable-features=WebRTCPipeWireCapturer'
 
-if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
+if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" || ${XDG_SESSION_TYPE:-} == "wayland" ]]
 then
-    EXTRA_ARGS="${EXTRA_ARGS} --enable-wayland-ime --ozone-platform-hint=auto --wayland-text-input-version=3"
+    EXTRA_ARGS="${EXTRA_ARGS} --enable-wayland-ime --wayland-text-input-version=3"
+
+    # work around electron's broken wayland detection
+    # TODO: remove when Slack uses an electron release that includes the fix
+    # https://github.com/electron/electron/pull/48301
+    EXTRA_ARGS="${EXTRA_ARGS} --ozone-platform=wayland"
 fi
 
 env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s ${EXTRA_ARGS} "$@"
