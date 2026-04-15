548cdb4464fdbe692f49b5ee9ff45f466bda862c
Alexander Koskovich <akoskovich@pm.me>
2026-04-15 16:48:25 -0400

    Default to enabling Wayland by default
    As of Slack 4.47.69, Wayland is now the default when available. No
    need to manually enable Wayland flags.
    
    Test: Share screen in a huddle on Ubuntu 26.04 w/NVIDIA on Wayland

--- slack.sh
+++ slack.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
-
-EXTRA_ARGS='--enable-features=WebRTCPipeWireCapturer'
-
-if [[ -e "${XDG_RUNTIME_DIR}/${WAYLAND_SOCKET}" || -e "${WAYLAND_DISPLAY}" ]]
-then
-    EXTRA_ARGS="${EXTRA_ARGS} --enable-wayland-ime --wayland-text-input-version=3"
-
-    # work around electron's broken wayland detection
-    # TODO: remove when Slack uses an electron release that includes the fix
-    # https://github.com/electron/electron/pull/48301
-    EXTRA_ARGS="${EXTRA_ARGS} --ozone-platform=wayland"
-fi
-
-env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s ${EXTRA_ARGS} "$@"
+env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s "$@"
