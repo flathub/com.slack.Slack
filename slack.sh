15bf76c2c2956ca5e917e3c56b359304b7761e20
Andri Thorlacius <andri.thorlacius@gmail.com>
2026-05-07 14:07:01 +0000

    slack.sh: quote variable expansions to handle paths with spaces
    env(1) word-splits unquoted command substitutions and parameter
    expansions in its NAME=VALUE arguments. When XDG_DOWNLOAD_DIR
    or XDG_CACHE_HOME contains a space (e.g. Icelandic 'Sóttar skrár',
    or any localized download folder name with whitespace), the second
    word is parsed as a positional argument and env(1) tries to execvp
    it as a command, failing with:
    
        env: 'skrár': No such file or directory
    
    Slack then never starts. Fixes the same class of failure reported in
    #313 (env: 'Temp/Google': No such file or directory) and is the root
    cause behind several reports in #52 from users with localized
    download folder names.
    
    Quote the expansions so the values are passed to env(1) as single
    arguments regardless of internal whitespace.

--- slack.sh
+++ slack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env TMPDIR=${XDG_CACHE_HOME} XDG_DOWNLOAD_DIR=$(xdg-user-dir DOWNLOAD) zypak-wrapper /app/extra/slack -s "$@"
+env TMPDIR="${XDG_CACHE_HOME}" XDG_DOWNLOAD_DIR="$(xdg-user-dir DOWNLOAD)" zypak-wrapper /app/extra/slack -s "$@"
